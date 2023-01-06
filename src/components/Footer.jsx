e15990e49c095f8b5af4b4da1c05349b8bb68907
vishusharmaX <sharmavishwajeet04@gmail.com>
2023-01-06 11:58:09 +0530

    firstcommit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechTrends</h1>
+        <p>@ all right reserved</p>
+     </div>  
+     <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href="https://www.linkedin.com/in/vishwajeet-sharma-649a21205" rel="noreferrer" target = {"_blank"}>linkedin</a>
+            <a href="https://github.com/vishusharmaX" rel="noreferrer" target = {"_blank"}>Github</a>
+            <a href="https://www.instagram.com/vishu.livincool/" target = {"_blank"} rel="noreferrer">Instagram</a>
+            <a href="https://twitter.com/VishuSh62450403?t=m-5QTgaVVlWHmsFXDuXxFw&s=09"  rel="noreferrer" target = {"_blank"}>Twitter</a>
+        </div>
+     </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Services.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Carousel} from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import img1 from "../assets/3.jpg";
+import img2 from "../assets/4.jpg";
+
+const Services = () => {
+  return (
+   <div>
+    <Carousel
+    infiniteLoop autoPlay showStatus ={false} showArrows={false}
+    interval = {1000}
+    showThumbs={false}
+    >
+        <div>
+            <img src={img1} alt="item1" />
+            <p className='legend'>Full stack</p>
+        </div>
+        <div>
+            <img src={img2} alt="item2" />
+            <p className='legend'>Peer-to-peer Support</p>
+        </div>
+    </Carousel>
+   </div>
+  )
+}
+
+export default Services
